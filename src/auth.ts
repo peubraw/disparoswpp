b85c155abbe3b62b09670c28c39ae3f241a3b3c3
peubraw <peubraw@gmail.com>
2026-05-21 23:29:42 -0300

    fix: add basePath to NextAuth v5 config for /disparoswpp subpath

--- src/auth.ts
+++ src/auth.ts
@@ -11,6 +11,7 @@ const credentialsSchema = z.object({
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
   trustHost: true,
+  basePath: process.env.NEXT_PUBLIC_BASE_PATH ?? "",
   providers: [
     Credentials({
       name: "credentials",
